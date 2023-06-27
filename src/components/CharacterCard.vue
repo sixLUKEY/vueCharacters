bdc19d07e50acf4c06aff53d6147146f3cc65bea
sixLUKEY <marcoslukerojo@gmail.com>
2023-06-27 12:46:47 +0200

    first commit

---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <h3>
+        Character Name
+    </h3>
+    <img src="" alt="Character Image">
+    <p>
+        Age: 
+    </p>
+    <p>
+        Height: 
+    </p>
+    <h4>
+        Akatsuki
+    </h4>
+
+</template>
+
+<script>
+    export default{
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/CharactersVuew.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>
+            Characters
+        </h2>
+    </div>
+
+    <div>
+        <CharacterCard/>
+    </div>
+</template>
+
+<script>
+    import CharacterCard from '@/components/CharacterCard.vue';
+
+    export default {
+        components: {
+            CharacterCard
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
